310c2ef64f825d7811187d6ae4955acdc9799f8f
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-12-28 16:57:44 -0300

    feat_claseItem * listamos cada item * añadimos estilos basicos

--- cliente/src/components/ClaseList.js
+++ cliente/src/components/ClaseList.js
@@ -1,14 +1,19 @@
 import { View, Text, FlatList } from "react-native";
 import React from "react";
 
-const ClaseList = ({clases}) => {
+import ClaseItem from "./ClaseItem";
+
+const ClaseList = ({ clases }) => {
+  const renderItem = ({ item }) => {
+    return <ClaseItem clase={item} />;
+  };
+
   return (
     <FlatList
+      style={{ width: "100%" }}
       data={clases}
-      renderItem={({ item }) => {
-        console.log(item);
-        return <Text>{item.title}</Text>;
-      }}
+      keyExtractor={(item) => item.id + ""}
+      renderItem={renderItem}
     />
   );
 };
