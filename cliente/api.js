108177555bbdf84f52687c54b6f83de220f393c4
jeronimo <huincamanjeronimoepet20@gmail.com>
2022-12-28 16:51:11 -0300

    funcion obtener clases *getClases

--- cliente/api.js
+++ cliente/api.js
@@ -1,7 +1,8 @@
-const API = "http://10.0.2.2:4000/clases";
+const API = 'http://192.168.1.47:4000/clases';
 
 export const getClases = async () => {
-  const res = await fetch(API);
-  const data = await res.json();
-  console.log(data);
+  const res = await fetch(API, {
+    method: "GET",
+  });
+  return await res.json();
 };
