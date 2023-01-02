bb7500254a4e4676121331304f0d51a19ff53804
jeronimo <huincamanjeronimoepet20@gmail.com>
2023-01-02 17:03:34 -0300

    API * saveClase * deleteClase

--- cliente/api.js
+++ cliente/api.js
@@ -1,8 +1,23 @@
-const API = 'http://192.168.1.47:4000/clases';
+const API = "http://192.168.1.47:4000/clases";
 
 export const getClases = async () => {
   const res = await fetch(API, {
     method: "GET",
   });
   return await res.json();
+};
+
+export const saveClase = async (newClase) => {
+  const res = await fetch(API, {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-type": "application/json" },
+    body: JSON.stringify(newClase),
+  });
+  return await res.json();
+};
+
+export const deleteClase = async (id) => {
+  await fetch(`${API}/${id}`, {
+    method: "DELETE",
+  });
 };
