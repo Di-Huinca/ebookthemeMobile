daccc28383333836bbb4d599e7a95dd67af1cb48
jeronimo <huincamanjeronimoepet20@gmail.com>
2023-01-02 17:09:16 -0300

    lista de clases * carga las clases al deslizar hacia abajo * carga las clases al eliminar un item

--- cliente/src/components/ClaseList.js
+++ cliente/src/components/ClaseList.js
@@ -1,12 +1,36 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import { View, Text, FlatList, RefreshControl } from "react-native";
+import React, { useState, useEffect } from "react";
+import { deleteClase, getClases } from "../../api";
 
 import ClaseItem from "./ClaseItem";
 
-const ClaseList = ({ clases }) => {
-  const renderItem = ({ item }) => {
-    return <ClaseItem clase={item} />;
+const ClaseList = () => {
+  const [refreshing, setRefreshing] = useState();
+  const [clases, setClases] = useState([]);
+
+  const loadClases = async () => {
+    const data = await getClases();
+    setClases(data);
   };
+
+  useEffect(() => {
+    loadClases();
+  }, []);
+
+  const handleDelete = async (id) => {
+    await deleteClase(id);
+    await loadClases();
+  };
+
+  const renderItem = ({ item }) => {
+    return <ClaseItem clase={item} handleDelete={handleDelete} />;
+  };
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
+    await loadClases();
+    setRefreshing(false);
+  });
 
   return (
     <FlatList
@@ -14,6 +38,13 @@ const ClaseList = ({ clases }) => {
       data={clases}
       keyExtractor={(item) => item.id + ""}
       renderItem={renderItem}
+      refreshControl={
+        <RefreshControl
+          onRefresh={onRefresh}
+          colors={["#78e08f"]}
+          refreshing={refreshing}
+        />
+      }
     />
   );
 };
