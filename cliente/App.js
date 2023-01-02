8ac3fb5e56068191929b59392c9db62315ccd6f7
jeronimo <huincamanjeronimoepet20@gmail.com>
2023-01-02 17:04:50 -0300

    App * Navegacion formulario de clases * estilos header de pantallas

--- cliente/App.js
+++ cliente/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
+import { Text, TouchableOpacity } from "react-native";
 //Importacion de screens
 import HomeScreen from "./screens/HomeScreen";
 import ClaseFormScreen from "./screens/ClaseFormScreen";
@@ -12,8 +12,43 @@ const App = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Clase" component={ClaseFormScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({
+            title: "Inicio",
+            headerStyle: {
+              backgroundColor: "#b2b2b2",
+            },
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+            headerRight: () => (
+              <TouchableOpacity
+                onPress={() => navigation.navigate("ClaseForm")}
+              >
+                <Text
+                  style={{ color: "#fffff", marginRight: 20, fontSize: 15 }}
+                >
+                  Apretame
+                </Text>
+              </TouchableOpacity>
+            ),
+          })}
+        />
+        <Tab.Screen
+          name="ClaseForm"
+          component={ClaseFormScreen}
+          options={{
+            title: "Nueva Clase",
+            headerStyle: {
+              backgroundColor: "#b2b2b2",
+            },
+            headerTitleStyle: {
+              color: "#ffffff",
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
